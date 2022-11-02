fd21063ff33780e824c023e465d3c8ee75db0e90
Renan Rey - Work <renan.reyc@gmail.com>
2022-11-01 21:52:12 -0300

    implementing firebase and managing participants in a room

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import dbRef, { connectedRef, userName } from './server/firebase';
 import './App.css';
+import { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { setUser, addParticipant, removeParticipant} from './store/actioncreator';
 
-function App() {
+function App(props) {
+  const participantRef = dbRef.child("participants");
+  useEffect(() => {
+    connectedRef.on('value', (snap) => {
+      if(snap.val()) {
+        const defaultPreferences = {
+          audio: true,
+          video: false,   
+          screen: false,
+        }
+        const userRef = participantRef.push({
+          userName,
+          preference: defaultPreferences,
+        });
+        
+        props.setUser({
+          [userRef.key]: {
+            userName,
+            ...defaultPreferences,
+          }, 
+        });
+        userRef.onDisconnect().remove();
+      }
+    });
+
+  }, []);
+
+  useEffect(() => {
+
+    if(props.user){
+      participantRef.on("child_added", (snap) => {
+        const { userName, preferences } = snap.val();
+        props.addParticipant({
+          [snap.key]: {
+            userName,
+            ...preferences,
+          },
+        });
+      });
+      participantRef.on("child_removed", (snap) => {
+        props.removeParticipant(snap.key);
+      });
+    }
+
+  }, [props.user]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Current user: { JSON.stringify(props.user)} <br/>
+      Participants: { JSON.stringify(props.participants) }
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    user: state.currentUser,
+    participants: state.participants,
+  }  
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setUser: (user) => dispatch(setUser(user)),
+    addParticipant: (participant) => dispatch(addParticipant(participant)),
+    removeParticipant: (participantKey) => dispatch(removeParticipant(participantKey)),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/server/firebase.js
@@ -0,0 +1,27 @@
+import firebase from "firebase/app";
+import database from "firebase/database";
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyD3rcUfw6uW8PzYUYUjyX7ns8hiGFwgAmc',
+    databaseURL: 'https://friend-eyes-default-rtdb.firebaseio.com/'
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let dbRef = firebase.database().ref();
+
+export let connectedRef = firebase.database().ref(".info/connected");
+
+export const userName = prompt("Qual é o seu nome?");
+
+const urlParams = new URLSearchParams(window.location.search);
+const roomId = urlParams.get("id");
+
+if(roomId) {
+    dbRef = dbRef.child(roomId);
+}else {
+    dbRef = dbRef.push();
+    window.history.replaceState(null, "Meet", "?id=" +dbRef.key);
+}
+
+export default dbRef;
